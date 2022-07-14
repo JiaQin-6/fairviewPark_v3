7addcecff71d677c13ecdc3c19d1166fd97be2fb
嘉嘉 <1723470065@qq.com>
2022-07-14 11:43:47 +0800

    添加boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 /*
  * @Author: 嘉嘉 51945758+JiaQin-6@users.noreply.github.com
  * @Date: 2022-07-13 22:03:21
- * @LastEditors: 嘉嘉 51945758+JiaQin-6@users.noreply.github.com
- * @LastEditTime: 2022-07-13 22:21:30
+ * @LastEditors: 嘉嘉 1723470065@qq.com
+ * @LastEditTime: 2022-07-14 11:04:11
  * @FilePath: /fairviewpark/Users/david/Desktop/vue3.0/fairviewPark/src/App.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld";
 
 export default defineComponent({
-    setup() {
-        return () => {
-            return (
-                <div>
-                    <div>
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
-                        </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-                        </a>
-                    </div>
-                    <HelloWorld msg="Hello Vue 3 + Vite8" />
-                </div>
-
-            )
-        }
+  emit: ["emit"],
+  setup(props,{emit}) {
+    console.log(emit('emit'));
+    const getData = (value) => {
+        console.log(value);
     }
+    return () => {
+      return (
+        <div>
+          <HelloWorld msg="向子组件传值" onEmit={getData}/>
+        </div>
+      );
+    };
+  },
 });
--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,32 +1,111 @@
 /*
  * @Author: 嘉嘉 51945758+JiaQin-6@users.noreply.github.com
  * @Date: 2022-07-13 22:23:40
- * @LastEditors: 嘉嘉 51945758+JiaQin-6@users.noreply.github.com
- * @LastEditTime: 2022-07-13 22:44:14
+ * @LastEditors: 嘉嘉 1723470065@qq.com
+ * @LastEditTime: 2022-07-14 11:41:05
  * @FilePath: /fairviewpark/Users/david/Desktop/vue3.0/fairviewPark/src/components/HelloWorld.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-    
-    setup() {
-        const count = ref(0)
-        return () => {
-            return (
-                <div>
-                    <div>
-                        <a href="https://vitejs.dev" target="_blank">
-                            <img src="/vite.svg" class="logo" alt="Vite logo" />
+  //接收父向子传递过来的值
+  props: ["msg"],
+  setup(props, { emit }) {
+    const count = ref(0);
+    console.log(count);
+    console.log(props.msg);
+    return () => {
+      return (
+        <div>
+          <p style={{ color: "red" }}>{props.msg}</p>
+          <p
+            onClick={() => {
+              emit("emit", "向父组件传值");
+            }}
+          >
+            点击我向父组件传值
+          </p>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                Navbar
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">
+                      Link
+                    </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a
+                      class="nav-link dropdown-toggle"
+                      href="#"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Action
                         </a>
-                        <a href="https://vuejs.org/" target="_blank">
-                            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Another action
                         </a>
-                    </div>
-                    <HelloWorld msg="Hello Vue 3 + Vite8" />
-                </div>
-
-            )
-        }
-    }
+                      </li>
+                      <li>
+                        <hr class="dropdown-divider" />
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Something else here
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button class="btn btn-outline-success" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
+      );
+    };
+  },
 });
